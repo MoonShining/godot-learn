078437842b3b89c82c933c8a496498a729923a83
ZhouBinJie <zhoubinjie@bytedance.com>
2026-05-09 16:09:04 +0800

    global class

--- base_chac.gd
+++ base_chac.gd
@@ -1,11 +1,16 @@
 extends CharacterBody2D
 
+class_name BaseCharacter
+
+var level = 1
+var hit_rate = 2
 
 @onready var body: AnimatedSprite2D = $Body
-@onready var weapon: Sprite2D = $Weapon
+@onready var weapon: AnimatedSprite2D = $"Body/Weapon"
 
 func _ready():
 	# 确保一开始进度是 0
+	# GlobalConfig.hello(GlobalConfig.Gravity)
 	set_shader_progress(0.0)
 	
 func _physics_process(delta: float) -> void:
@@ -14,25 +19,12 @@ func _physics_process(delta: float) -> void:
 
 # 假设这个函数在玩家血量归零时调用
 func die():
-	## 1. 播放死亡动画（假设动画名叫 "death"）
-	#play("death")
-#
-	## 2. 等待死亡动画播放完毕
-	#await animation_finished
-
-	# 或者如果你不想播放动画，而是立刻在当前帧碎裂，可以注销上面两行，直接执行下面。
-
-	# 3. 开始爆炸消散效果
 	explode_effect()
 
 func explode_effect():
-	# 创建一个补间动画 (Tween)
 	var tween = create_tween()
-
 	# 用 1.0 秒的时间，将 shader 里的 progress 从 0.0 变到 1.0
-	tween.tween_method(set_shader_progress, 0.0, 1.0, 1.0).set_trans(Tween.TRANS_SINE)
-
-	# 爆炸效果结束后，销毁这个节点（或者整个角色实例）
+	tween.tween_method(set_shader_progress, 0.0, 1.0, 0.5).set_trans(Tween.TRANS_SINE)
 	tween.tween_callback(queue_free)
 
 # Tween 调用的回调函数，用于更新 Shader 的值
---
+++ global.gd
@@ -0,0 +1,7 @@
+extends Node
+
+@export var Gravity = 908
+
+# Called when the node enters the scene tree for the first time.
+func hello(n: int) -> void:
+	print("hello global", n) # Replace with function body.
